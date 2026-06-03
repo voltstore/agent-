70730cf7032d268eb34a620264e604bb3f3bfb6a
voltstore <voltstore301@gmail.com>
2026-06-03 11:09:40 +0300

    fix STATUSES

---
+++ fix.js
@@ -0,0 +1,1 @@
+const fs=require(chr(39)+" fs+chr(39));
---
+++ fix.py
@@ -0,0 +1,5 @@
+﻿import re
+c=open('index.html','r',encoding='utf-8').read()
+c=re.sub(r'STATUSES=\{[^}]+\}','STATUSES={' + "new:'new',sent:'sent',reply:'reply',client:'client'" + '}',c)
+open('index.html','w',encoding='utf-8').write(c)
+print('done')
